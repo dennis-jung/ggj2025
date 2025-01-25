f9eae2a63e60e31f39eb19ff3223d1f8edd3cf69
Dennis Jung <dmjung@live.de>
2025-01-25 11:23:36 +0100

    portals changed, to not use direct reference

--- scenes/tileset_test/game.gd
+++ scenes/tileset_test/game.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-@onready var sticky: Sticky = $Sticky
 @onready var upstairs_spawn_point: Marker2D = $Map/UpstairsSpawnPoint
 
 func _on_portal_body_entered(body: Node2D) -> void:
-	if body == sticky:
-		sticky.position = upstairs_spawn_point.position
+	if body.is_in_group("Player"):
+		body.position = upstairs_spawn_point.position
