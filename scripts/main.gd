f8e83734875676f328bf565b5749c51be53a3426
Dennis Jung <dmjung@live.de>
2025-01-25 10:48:02 +0100

    added coin, main script

---
+++ scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var starting_fuel: float = 100.0
+
+@onready var coins: int = 0
+@onready var current_fuel: float = starting_fuel
+
+func on_fuel_modified(difference: float) -> void:
+	current_fuel += difference
+	
+func on_coin_pickup() -> void:
+	coins += 1
