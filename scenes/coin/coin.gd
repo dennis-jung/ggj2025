7ecbe7ec5d23e701039b09eed7803f971d6caf05
Dennis Jung <dmjung@live.de>
2025-01-25 12:19:22 +0100

    removed unnecassary logs in coin

--- scenes/coin/coin.gd
+++ scenes/coin/coin.gd
@@ -13,7 +13,6 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		print("Player picked me up!")
 		# add point in main
 		Main.on_coin_pickup()
 		# destroy myself
