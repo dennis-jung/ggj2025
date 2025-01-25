0ba011e625798896705b026cae4f0cacf2ef9f32
siabrac <florian.wuensche@live.com>
2025-01-25 11:30:48 +0100

    moved all sticky physics code to FSM states

--- scenes/sticky/idle.gd
+++ scenes/sticky/idle.gd
@@ -13,7 +13,22 @@ func update(_delta: float) -> void:
 	pass
 
 func update_physics(_delta: float) -> void:
+	# Add the gravity.
+	if not parent.is_on_floor():
+		parent.velocity += parent.get_gravity() * _delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and parent.is_on_floor():
+		parent.velocity.y = parent.JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		parent.velocity.x = direction * parent.SPEED
+	else:
+		parent.velocity.x = move_toward(parent.velocity.x, 0, parent.SPEED)
+
 	if parent.velocity.length() > 0:
 		state_transition.emit("walking", self)
 	parent.move_and_slide()
-#func _physics_process(delta: float) -> void:
--- scenes/sticky/walking.gd
+++ scenes/sticky/walking.gd
@@ -10,6 +10,22 @@ func enter():
 
 
 func update_physics(_delta: float):
+	# Add the gravity.
+	if not parent.is_on_floor():
+		parent.velocity += parent.get_gravity() * _delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and parent.is_on_floor():
+		parent.velocity.y = parent.JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		parent.velocity.x = direction * parent.SPEED
+	else:
+		parent.velocity.x = move_toward(parent.velocity.x, 0, parent.SPEED)
+		
 	parent.move_and_slide()
 	update_state()
 
