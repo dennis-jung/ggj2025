7c8e3f966318e8fe3f7ffc66fcaa04cfd1da2abb
siabrac <florian.wuensche@live.com>
2025-01-26 12:04:13 +0100

    added level exits

--- scenes/levels/level_001/level_001.gd
+++ scenes/levels/level_001/level_001.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 
 @onready var upstairs_spawn_point: Marker2D = $UpstairsSpawnPoint
-@onready var ui: Ui = $UI
 
 
 func _on_portal_downstairs_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		var sticky: Sticky = body
 		sticky.port(upstairs_spawn_point.position)
+
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	Main.next_level()
