ecd5165246718e4d997ac9533a5cbd547a563581
siabrac <florian.wuensche@live.com>
2025-01-26 14:22:18 +0100

    subtract deaths from coins at game end

--- scenes/levels/End/end.gd
+++ scenes/levels/End/end.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 const BUBBLE_Y_POS_MIN = 1000
 const BUBBLE_Y_POS_MAX = 300
-const MAX_COINS = 100.0
+const MAX_COINS = 1000.0
+const COINS_PER_DEATH = 50
 
 @onready var bubble_sprite: Sprite2D = $BubbleSprite
 @onready var label_coins: Label = $UI/LabelCoins
+@onready var label_deaths: Label = $UI/LabelDeaths
 @onready var label_text_025: Label = $UI/LabelText025
 @onready var label_text_050: Label = $UI/LabelText050
 @onready var label_text_075: Label = $UI/LabelText075
@@ -13,7 +15,7 @@ const MAX_COINS = 100.0
 @onready var coin_label
 
 var coin_display: int = 0
-
+var deaths_display: int = 0
 
 func _ready() -> void:
 	start_bubble()
@@ -21,14 +23,11 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	label_coins.text = "Coins: " + str(coin_display)
-	if coin_display >= 25:
-		label_text_025.visible = true
-	if coin_display >= 50:
-		label_text_050.visible = true
-	if coin_display >= 75:
-		label_text_075.visible = true
-	if coin_display >= 100:
-		label_text_100.visible = true
+	label_deaths.text = "Deaths: " + str(deaths_display)
+	label_text_025.visible = coin_display >= MAX_COINS * 0.25
+	label_text_050.visible = coin_display >= MAX_COINS * 0.5
+	label_text_075.visible = coin_display >= MAX_COINS * 0.75
+	label_text_100.visible = coin_display >= MAX_COINS
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
@@ -39,11 +38,29 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 
 func start_bubble() -> void:
-	Main.coins = 100
-	var coins = clamp(Main.coins, 0, 100)
-	var target_pos_y = BUBBLE_Y_POS_MIN - (BUBBLE_Y_POS_MIN - BUBBLE_Y_POS_MAX) * (Main.coins / MAX_COINS)
+	Main.coins = 400
+	Main.deaths = 10
+	var coins = clamp(Main.coins, 0, MAX_COINS)
+	var target_pos_y = BUBBLE_Y_POS_MIN - (BUBBLE_Y_POS_MIN - BUBBLE_Y_POS_MAX) * (coins / MAX_COINS)
 	var target_pos = Vector2(bubble_sprite.position.x, target_pos_y)
 	var duration = 5.0 * (Main.coins / MAX_COINS)
 	var tween = create_tween()
 	tween.tween_property(bubble_sprite, "position", target_pos, duration)
-	tween.parallel().tween_property(self, "coin_display", Main.coins, duration) 
+	tween.parallel().tween_property(self, "coin_display", Main.coins, duration)
+	await tween.finished
+	print_debug("tween.finished")
+	tween.kill()
+	await get_tree().create_timer(1).timeout
+	label_deaths.visible = true
+	var coins_after_deaths = coins - Main.deaths * COINS_PER_DEATH
+	print_debug("coins before death: " + str(coins))
+	print_debug("coins after deaths: " + str(coins_after_deaths))
+	target_pos_y = BUBBLE_Y_POS_MIN - (BUBBLE_Y_POS_MIN - BUBBLE_Y_POS_MAX) * (coins_after_deaths / MAX_COINS)
+	print_debug("target_pos after deaths: " + str(target_pos_y))
+	target_pos = Vector2(bubble_sprite.position.x, target_pos_y)
+	duration = 3.0 * ((coins - coins_after_deaths) / MAX_COINS)
+	print_debug("duration after deaths: " + str(duration))
+	tween = create_tween()
+	tween.tween_property(bubble_sprite, "position", target_pos, duration)
+	#tween.parallel().tween_property(self, "coin_display", coins_after_deaths, duration)
+	tween.parallel().tween_property(self, "deaths_display", Main.deaths, duration)
